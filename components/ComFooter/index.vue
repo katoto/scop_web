a8caa9f18d9e8a8ad915d8aeb09c2944b4f4c62d
Davs <easyvalley@163.com>
2025-05-08 21:33:40 +0800

    update

--- components/ComFooter/index.vue
+++ components/ComFooter/index.vue
@@ -2,12 +2,14 @@
 const localePath = useLocalePath();
 let { t } = useI18n()
 
+
+
 const navigation = {
     main: [
-        { name: t('quickLinks.home'), href: '/' },
-        { name: t('quickLinks.solutions'), href: '/solutions' },
-        { name: t('quickLinks.research'), href: '/research' },
-        { name: t('quickLinks.about'), href: '/about' },
+        { name: t('footer.quickLinks.home'), href: '/' },
+        { name: t('footer.quickLinks.solutions'), href: '/solutions' },
+        { name: t('footer.quickLinks.research'), href: '/research' },
+        { name: t('footer.quickLinks.about'), href: '/about' },
     ],
     social: [
         {
@@ -27,22 +29,25 @@ const navigation = {
         },
     ],
 };
+console.log(t('quickLinks.home'), '城市')
+console.log(navigation, '==navigation')
 </script>
 
 <template>
     <footer class="bg-gray-900 text-white" :style="{ background: 'pink' }">
         <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
             <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+                {{ navigation.main }}
                 <div :key="item.name" class="pb-6" v-for="item in navigation.main">
-                    <Link href={item.href} class="text-sm leading-6 text-gray-300 hover:text-white">
-                    {{ item.name }}
+                    <Link :href="item.href" class="text-sm leading-6 text-gray-300 hover:text-white">
+                    {{ item.name }}===name
                     </Link>
                 </div>
             </nav>
             <div class="mt-10 flex justify-center space-x-10">
                 <div v-for="item in navigation.social">
-                    <Link key={item.name} href={item.href} class="text-gray-400 hover:text-gray-300">
-                    <span class="sr-only">{item.name}</span>
+                    <Link :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-300">
+                    <span class="sr-only">{{ item.name }}</span>
                     <item.icon class="h-6 w-6" aria-hidden="true" />
                     </Link>
                 </div>
