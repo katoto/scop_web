56ca235e160d6f50fb5c981d6091e9192db585a9
Davs <easyvalley@163.com>
2025-05-08 20:20:46 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-
+  devServer: {
+    port: 3001,
+  },
   modules: ["@nuxtjs/i18n"],
   i18n: {
     strategy: "prefix_except_default",
