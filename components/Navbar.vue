5c9e94e369e0a4926b5f0790a4e30cbc0fb5af0c
Davs <easyvalley@163.com>
2025-05-09 21:58:29 +0800

    feat: 切换语言

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,11 +4,11 @@
       <NuxtLink class="navbar-brand" :to="localePath('/')">
         <img src="/images/logo.png" alt="Logo" height="40">
       </NuxtLink>
-      
+
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
         <span class="navbar-toggler-icon"></span>
       </button>
-      
+
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto">
           <li class="nav-item dropdown">
@@ -16,10 +16,22 @@
               {{ $t('nav.solutions') }}
             </a>
             <ul class="dropdown-menu">
-              <li><NuxtLink class="dropdown-item" :to="localePath('/solution/passenger')">{{ $t('nav.passenger') }}</NuxtLink></li>
-              <li><NuxtLink class="dropdown-item" :to="localePath('/solution/commercialEV')">{{ $t('nav.commercialEV') }}</NuxtLink></li>
-              <li><NuxtLink class="dropdown-item" :to="localePath('/solution/energyStorage')">{{ $t('nav.energyStorage') }}</NuxtLink></li>
-              <li><NuxtLink class="dropdown-item" :to="localePath('/solution/recycling')">{{ $t('nav.recycling') }}</NuxtLink></li>
+              <li>
+                <NuxtLink class="dropdown-item" :to="localePath('/solution/passenger')">{{ $t('nav.passenger') }}
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink class="dropdown-item" :to="localePath('/solution/commercialEV')">{{ $t('nav.commercialEV') }}
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink class="dropdown-item" :to="localePath('/solution/energyStorage')">{{ $t('nav.energyStorage')
+                  }}</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink class="dropdown-item" :to="localePath('/solution/recycling')">{{ $t('nav.recycling') }}
+                </NuxtLink>
+              </li>
             </ul>
           </li>
           <li class="nav-item">
@@ -38,15 +50,16 @@
             <NuxtLink class="nav-link" :to="localePath('/contact')">{{ $t('nav.contact') }}</NuxtLink>
           </li>
         </ul>
-        
+
         <!-- Language Switcher -->
         <div class="dropdown">
-          <button class="btn btn-link dropdown-toggle text-dark" type="button" id="languageDropdown" data-bs-toggle="dropdown">
+          <button class="btn btn-link dropdown-toggle text-dark" type="button" id="languageDropdown"
+            data-bs-toggle="dropdown">
             {{ currentLocale.name }}
           </button>
           <ul class="dropdown-menu dropdown-menu-end">
             <li v-for="locale in availableLocales" :key="locale.code">
-              <NuxtLink class="dropdown-item" :to="switchLocalePath($route.fullPath, locale.code)">
+              <NuxtLink class="dropdown-item" :to="switchLocalePath(locale.code)">
                 {{ locale.name }}
               </NuxtLink>
             </li>
@@ -75,19 +88,19 @@ const currentLocale = computed(() => {
 <style lang="less" scoped>
 .navbar {
   padding: 1rem 0;
-  
+
   .nav-link {
     font-weight: 500;
     padding: 0.5rem 1rem;
-    
+
     &:hover {
       color: #007bff;
     }
   }
-  
+
   .dropdown-toggle {
     text-decoration: none;
-    
+
     &:after {
       margin-left: 0.5rem;
     }
@@ -95,15 +108,15 @@ const currentLocale = computed(() => {
 
   .dropdown-menu {
     border: none;
-    box-shadow: 0 2px 10px rgba(0,0,0,0.1);
-    
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+
     .dropdown-item {
       padding: 0.5rem 1.5rem;
-      
+
       &:hover {
         background-color: #f8f9fa;
       }
     }
   }
 }
-</style> 
+</style>
