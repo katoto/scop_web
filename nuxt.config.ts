eb4210527795cb333468fd500f7cda53096b9ba7
Davs <easyvalley@163.com>
2025-06-14 07:58:05 +0800

    SCOPHIL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,7 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
-      title: "Commercial EV Solutions",
+      title: "SCOPHIL",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
